8d7e60711858681b6be5478dd05cd403b6332129
Angela <89052245+serenitysprayer@users.noreply.github.com>
2024-09-25 09:27:20 -1000

    first commit

---
+++ src/main/java/com/example/taskmanagementDB/TaskmanagementDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskmanagementDB/TaskmanagementDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagementDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
